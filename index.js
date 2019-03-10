453ff8ac72e3d0f684116943c1214a9d4e0883fc
tjdavenport <tjdavenport2@gmail.com>
2019-03-10 19:27:04 -0400

    async generator, over 9000 power level

--- index.js
+++ index.js
@@ -15,28 +15,55 @@ new Builder()
   .build()
   .then(async d => {
     try {
-      await d.get('https://www.instagram.com/tjdavenport2/?hl=en');
-      const images = await d.findElements(By.css('article > div > div img'));
-      const sources = await Promise.all(images.map(image => image.getAttribute('src')));
-      await Promise.all(sources.map(source => {
-        return new Promise((resolve, reject) => {
-          const writer = fs.createWriteStream(path.join('out', `${i++}.jpg`));
+      await d.get('https://www.instagram.com/javascript.js/?hl=en');
+      const writes = [];
 
-          writer.on('error', err => {
-            reject(err);
-          });
+      for await (const sources of async function*() {
+        while (true) {
+          const images = await d.findElements(By.css('article > div > div img'));
 
-          https.get(source, res => {
-            writer.on('finish', () => {
-              writer.close(resolve);
+          if (images.length) {
+            const sources = await Promise.all(images.map(image => image.getAttribute('src')));
+            await d.executeScript('window.scrollTo(0,document.body.scrollHeight);');
+            await d.executeScript(sources.map(source => {
+              return `document.querySelector('[src="${source}"]').remove()`;
+            }).join(';'));
+
+            let attempts = 0;
+            await d.wait(() => {
+              return d.findElements(By.css('article > div > div img'))
+                .then(elements => {
+                  attempts++;
+                  return (elements.length > 0) || (attempts === 200);
+                });
+            });
+            yield(sources);
+          } else {
+            break;
+          }
+        }
+      }()) {
+        sources.forEach(source => {
+          writes.push(new Promise((resolve, reject) => {
+            const writer = fs.createWriteStream(path.join('out', `${i++}.jpg`));
+
+            writer.on('error', err => {
+              reject(err);
             });
 
-            res.pipe(writer);
-          }).on('error', err => {
-            reject(err);
-          });
+            https.get(source, res => {
+              writer.on('finish', () => {
+                writer.close(resolve);
+              });
+
+              res.pipe(writer);
+            }).on('error', err => {
+              reject(err);
+            });
+          }));
         });
-      }));
+      }
+      await Promise.all(writes);
     } catch (err) {
       console.log(err);
     } finally {
